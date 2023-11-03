ce56f5b82081c9c9266f7c79d48e7446806ee6b4
jihannavitri <144442218+jihannavitri@users.noreply.github.com>
2023-11-03 21:27:50 +0700

    Update cashier.py

--- cashier.py
+++ cashier.py
@@ -2,26 +2,26 @@
 tr127 = Transaction()
 
 # menambahkan barang ke daftar belanja
-transaction1.add_item('Pasts Gigi', 3, 15000)
-transaction1.add_item('Sabun', 1, 25000)
-transaction1.add_item('Buku Tulis', 5, 6000)
+tr127.add_item('Pasts Gigi', 3, 15000)
+tr127.add_item('Sabun', 1, 25000)
+tr127.add_item('Buku Tulis', 5, 6000)
 
 # mengubah nama item Pasta Gigi
-transaction1.update_item_name('Pasts Gigi', 'Pasta Gigi')
+tr127.update_item_name('Pasts Gigi', 'Pasta Gigi')
 
 # Mengubah jumlah item sabun menjadi 2
-transaction1.update_item_qty('Sabun', 2)
+tr127.update_item_qty('Sabun', 2)
 
 # mengubah harga item Sabun dan Buku Tulis
-transaction1.update_item_price('Sabun', 32000)
-transaction1.update_item_price('Buku Tulis', 7000)
+tr127.update_item_price('Sabun', 32000)
+tr127.update_item_price('Buku Tulis', 7000)
 
 # menghapus item pasta gigi
-transaction1.delete_item('Pasta Gigi')
+tr127.delete_item('Pasta Gigi')
 
 # menambahkan item baru
-transaction1.add_item('Lampu', 3, 48000)
+tr127.add_item('Lampu', 3, 48000)
 
 # cek daftar belanja dan menghitung total harga
-transaction1.check_order()
-transaction1.total_price()
+tr127.check_order()
+tr127.total_price()
